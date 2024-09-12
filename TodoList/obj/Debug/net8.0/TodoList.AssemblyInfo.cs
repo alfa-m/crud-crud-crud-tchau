314e1405af6634519eae9e52dc60b2ccd17dbbda
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-12 13:43:29 -0300

    add swagger

--- TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650696404fdfb48e7195b6f2d7d66b72c0d8239f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6d3dee634495a507f9b052fe93a81fbc0606be")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
