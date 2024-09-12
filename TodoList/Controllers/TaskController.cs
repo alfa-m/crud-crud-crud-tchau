d6e9cc092a68d0938d62b9fe0e2b1b642148a939
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-12 12:38:34 -0300

    add TaskController

---
+++ TodoList/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+using TodoList.Models;
+using TodoList.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TaskController : ControllerBase
+{
+
+}
