15d3a0d2a7158a3eceae1a8b0cffa8970c4ee4bb
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-18 20:17:06 -0300

    injeta a dependencia à TarefaContext, usando SQLite, e aponta para o BD TodoList.db, que será criado

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,3 +1,5 @@
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<TarefaContext>("Data Source=TodoList.db");
 
 var app = builder.Build();
 
