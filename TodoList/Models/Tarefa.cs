6b0248b0da4c7a6c4a95f4eb800e87b0943172e5
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-18 20:27:48 -0300

    add DataAnnotations

--- TodoList/Models/Tarefa.cs
+++ TodoList/Models/Tarefa.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoList.Models;
 
 public class Tarefa
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string Conteudo { get; set; }
     public bool EstaFeita { get; set; }
 }
--- TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce03afc4ec398c7552d1e4646a1490d9e933ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2c2c06c218f05adc5d8ff417ee043d39bc569d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
