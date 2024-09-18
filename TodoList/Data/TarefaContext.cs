abeff047a05564a8f427b7cef7717ed81c260342
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-18 20:10:28 -0300

    finaliza TarefaContext.cs

--- TodoList/Data/TarefaContext.cs
+++ TodoList/Data/TarefaContext.cs
@@ -5,5 +5,11 @@ namespace TodoList.Data;
 
 public class TarefaContext : DbContext
 {
+    public TarefaContext(DbContextOptions<TarefaContext> options)
+        : base(options)
+    {
 
+    }
+
+    public DbSet<Tarefa> Todo => Set<Tarefa>();
 }
