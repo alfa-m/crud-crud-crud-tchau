929e47a2961b4581350e4eaf468321b242d5fda4
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-19 09:55:07 -0300

    atualiza imgagem Swagger

--- TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e7feeeb63e1999d93af93d4240390130588acf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14fade26e3bfc6a31ec001cc7b5a229a92262a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
