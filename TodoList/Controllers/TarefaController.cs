ad5414cb5452dd3640ef38bdb371afab9a538500
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-19 16:28:15 -0300

    Revert "muda rota principal"
    This reverts commit 7660948ad06f4b79cbdaf5e9f05ab477fd37bf03.

--- TodoList/Controllers/TarefaController.cs
+++ TodoList/Controllers/TarefaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TodoList.Controllers;
 
 [ApiController]
-[Route("todo")]
+[Route("[controller]")]
 public class TarefaController : ControllerBase
 {
     TarefaService _service;
